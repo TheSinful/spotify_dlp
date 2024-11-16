2e2735bac4e7293d2b4d7ffc48def37ab67510ba
WhiteKSI <email@gmail.com>
2024-11-16 13:34:28 -0600

    re-addition of yt-dlp binary data after fucking up git-lfs

---
+++ src/dlp/youtube/data/yt-dlp-data-linux.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e88a7c9cdae7613007889fee0a8cab252bd75715bcacecd9d3281467203da97
+size 104499775
---
+++ src/dlp/youtube/data/yt-dlp-data-windows.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72dce6d3dbea3b6cd91b4cde49aafc1340b49384c5aa7b158d4350b15f13933a
+size 59294279
