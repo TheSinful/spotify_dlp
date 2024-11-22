19336af673176e07c3159e708124ae82fe44352e
WhiteKSI <email@gmail.com>
2024-11-21 20:18:05 -0600

    Untested yt-dlp raw command execution and conversion of certain config fields to strings

--- src/dlp/youtube/yt-dlp.cpp
+++ src/dlp/youtube/yt-dlp.cpp
@@ -3,6 +3,7 @@
 #else
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #endif
 
 #include <fstream>
@@ -13,8 +14,8 @@
 
 YtDLP::YtDLP() : program_name(_WIN32 ? "yt_dlp.exe" : "yt_dlp")
 {
-    get_temp_path();
-    
+    this->get_temp_path();
+
 #ifdef _WIN32
     this->extract_yt_dlp_windows();
 #else
@@ -56,7 +57,7 @@ void YtDLP::get_temp_path()
     GetTempPathW(MAX_PATH, temp_dir);
     this->yt_dlp_temp_path = std::filesystem::path(temp_dir) / this->program_name;
 #else
-    std::string temp_path = "/tmp/" + this->program_name;  // Added missing slash
+    std::string temp_path = "/tmp/" + this->program_name; // Added missing slash
     this->yt_dlp_temp_path = std::filesystem::path(temp_path);
 #endif
 }
@@ -64,4 +65,88 @@ void YtDLP::get_temp_path()
 std::string YtDLP::get_path() const
 {
     return this->yt_dlp_temp_path.string();
+}
+
+bool YtDLP::command_return_ok(YtDLPExitCodes code)
+{
+    switch (code)
+    {
+    case 0:
+        return true;
+    default:
+        return false;
+    }
+}
+
+CommandResult YtDLP::execute_command(const std::string &command)
+{
+    CommandResult result;
+    std::array<char, 128> buffer;
+
+    std::string cmd = command + " 2>&1";
+
+#ifdef _WIN32
+    FILE *pipe = _popen(cmd.c_str(), "r");
+#else
+    FILE *pipe = popen(cmd.c_str(), "r");
+#endif
+
+    if (!pipe)
+    {
+        std::string log = "Failed to execute command: " + command;
+        THROW_AND_LOG(std::runtime_error, log, log);
+    }
+
+    // read out
+    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr)
+    {
+        result.out += buffer.data();
+    }
+
+#ifdef _WIN32
+    result.cmd_exit_code = _pclose(pipe);
+#else
+    result.cmd_exit_code = pclose(pipe);
+#endif
+
+    // shift right by 8 to get actual exit code on windows
+#ifdef _WIN32
+    result.ytdlp_exit_code = static_cast<YtDLPExitCodes>(result.cmd_exit_code >> 8);
+#else
+    result.ytdlp_exit_code = static_cast<YtDLPErrorCodes>(WEXITSTATUS(result.cmd_exit_code));
+#endif
+
+    return result;
+}
+
+void YtDLP::download(DownloadConfig config)
+{
+    this->config = config;
+}
+
+std::string YtDLP::convert_download_file_type()
+{
+    switch (this->config.download_file_type)
+    {
+    case DownloadFileType::MP3:
+        return "mp3";
+    case DownloadFileType::M4A:
+        return "m4a";
+    case DownloadFileType::OPUS:
+        return "opus";
+    case DownloadFileType::VORBIS:
+        return "vorbis";
+    case DownloadFileType::WAV:
+        return "wav";
+    case DownloadFileType::BEST:
+        return "best";
+    case DownloadFileType::AAC:
+        return "aac";
+    case DownloadFileType::ALAC:
+        return "alac";
+    case DownloadFileType::FLAC:
+        return "flac";
+    default:
+        return "best";
+    }
 }
--- src/dlp/youtube/yt-dlp.h
+++ src/dlp/youtube/yt-dlp.h
@@ -7,6 +7,24 @@
 #endif
 
 #include <filesystem>
+#include "../../../include/spotify-dlp.h"
+
+enum YtDLPExitCodes
+{
+    Success = 0,
+    UpdateRequired = 100,
+    CancelledByMaxDownloads = 101,
+    UserOptionsError = 2,
+    GenericError = 1,
+};
+
+struct CommandResult
+{
+    int cmd_exit_code;
+    YtDLPExitCodes ytdlp_exit_code;
+    std::string out;
+    std::string err;
+};
 
 class YtDLP
 {
@@ -16,15 +34,22 @@ class YtDLP
 
 public:
     YtDLP();
+    void download(DownloadConfig config);
 
     std::string get_path() const;
+
 private:
+    std::string convert_download_file_type();
     void extract_yt_dlp_windows();
     void extract_yt_dlp_linux();
     void get_temp_path();
+    CommandResult execute_command(const std::string &command);
+    bool command_return_ok(YtDLPExitCodes code);
 
     std::filesystem::path yt_dlp_temp_path;
     std::string program_name;
+
+    DownloadConfig config;
 };
 
 #endif
